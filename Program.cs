ba362618b7b12fff126f9fa842dea720cda06e02
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-05-17 18:36:18 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿# EXAnaliseCredito
+
 /* Exercício `AnaliseCredito`  Um cliente quer solicitar um empréstimo. Receba o valor solicitado, a quantidade de parcelas desejada e a renda mensal comprovada. Só autorize empréstimos cuja parcela não ultrapasse 30% da renda (desconsidere os juros). */
 int valorSolicitado, quantidadeParcela, rendaMensal, valorParcela;
 Console.Write("Digite o valor solicitado: ");
