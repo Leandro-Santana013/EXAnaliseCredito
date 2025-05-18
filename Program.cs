b7487657b977a13a2a17f1bda4f5267cbc47adac
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-05-17 18:34:09 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,16 +2,16 @@
 /* Exercício `AnaliseCredito`  Um cliente quer solicitar um empréstimo. Receba o valor solicitado, a quantidade de parcelas desejada e a renda mensal comprovada. Só autorize empréstimos cuja parcela não ultrapasse 30% da renda (desconsidere os juros). */
 int valorSolicitado, quantidadeParcela, rendaMensal, valorParcela;
 Console.Write("Digite o valor solicitado: ");
-
 valorSolicitado = int.Parse(Console.ReadLine()!);
-Console.Write("Digite a quantidade de parcelas: ");
 
+Console.Write("Digite a quantidade de parcelas: ");
 quantidadeParcela = int.Parse(Console.ReadLine()!);
 
 Console.Write("Digite a renda mensal: ");
 rendaMensal = int.Parse(Console.ReadLine()!);
 
 valorParcela = valorSolicitado / quantidadeParcela;
+
 if (valorParcela <= (rendaMensal * 0.3))
 {
     Console.WriteLine($"Empréstimo autorizado! O valor da parcela é {valorParcela}");
